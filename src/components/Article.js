82a45ecbb70f0afb7559683dae9960adef971ba8
Ant Newman <antnewman@ants-mbp.home>
2021-05-16 13:29:35 +0100

    temp fix for case studies

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useHistory } from 'react-router'
 import ContentBlock from './ContentBlock'
 
-export default function Article({ title, chunks }) {
+export default function Article({ title, chunks, children }) {
     const history = useHistory()
 
     useEffect(() => {
@@ -15,6 +15,7 @@ export default function Article({ title, chunks }) {
             <h1 className="content__block__heading">{title}</h1>
             <p><i><strong>By Martin C. Agunwa</strong></i></p>
             { chunks ? chunks : null }
+            { children ? children : null }
         </ContentBlock>
     )
 }
