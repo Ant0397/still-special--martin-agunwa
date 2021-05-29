371048188d5712c6064b53a41005742120c59261
Ant Newman <antnewman@Ants-MacBook-Pro.local>
2021-05-29 13:59:24 +0100

    header border

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,13 @@ import NavbarBrand from './nav/NavbarBrand'
 export default function Header() {
 
     return (
-        <div className={'header'}>
-            <NavbarBrand title="Chukwuemeka Martin Agunwa" subtitle="Think. Lead. Inspire" />
-            <NavProvider>
-                <Nav />
-            </NavProvider>
+        <div className="header__block">
+            <div className="header">
+                <NavbarBrand title="Chukwuemeka Martin Agunwa" subtitle="Think. Lead. Inspire" />
+                <NavProvider>
+                    <Nav />
+                </NavProvider>
+            </div>
         </div>
     )
 }
