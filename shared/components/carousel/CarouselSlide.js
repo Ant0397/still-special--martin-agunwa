7195250c4266b3628c39f0c4a6c16a7ca5ae1895
Ant Newman <antnewman@Ants-MacBook-Pro.local>
2021-06-20 20:23:07 +0100

    carousel issue with video fixed

--- shared/components/carousel/CarouselSlide.js
+++ shared/components/carousel/CarouselSlide.js
@@ -6,7 +6,7 @@ export default function CarouselSlide({ theme, blog, video, image }) {
     return (
         <div className="carousel__strip">
             <ContentCard carouselStrip theme={theme} title={blog.title} type="image" content={blog} />
-            <ContentCard carouselStrip theme={theme} title={video.title} type="video" content={video} /> 
+            <ContentCard carouselStrip theme={theme} title={video.title} type="video" content={video.url} /> 
             <ContentCard carouselStrip theme={theme} type="image" content={image} />
         </div>
     )
