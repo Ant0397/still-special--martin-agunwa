278f22e2a0a76b4ad40f6286b2a162d38819bee7
Ant Newman <antnewman@Ants-MacBook-Pro.local>
2021-06-20 19:42:53 +0100

    trying to fix ignore styles issue

--- webpack.server.js
+++ webpack.server.js
@@ -19,7 +19,6 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
                 use: 'babel-loader'
             },
             {
